0d9fa640d688a5aa4cd010a98079cac72195d36d
WilGero <wil.gero.2020@gmail.com>
2023-04-27 22:37:31 -0400

    first component and add vue router

--- src/components/CounterComponent.vue
+++ src/components/CounterComponent.vue
@@ -1,13 +1,27 @@
 <template>
+    <div>
+        
   <h2>Holaaa desde el componente Counter</h2>
+  <button class="btn btn-danger" @click="disminuir">-</button>
+        {{ contador }}
+  <button class="btn btn-primary" @click="aumentar">+</button>
+</div>
 </template>
 
 <script>
 export default {
-    name:Counter,
+    name:'CounterComponent',
     data(){
         return{
-
+            contador:0
+        }
+    },
+    methods:{
+        aumentar(){
+            this.contador++;
+        },
+        disminuir(){
+            this.contador--;
         }
     }
 }
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <counter-component></counter-component>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import CounterComponent from '@/components/CounterComponent.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+    CounterComponent
+  }
+}
+</script>
