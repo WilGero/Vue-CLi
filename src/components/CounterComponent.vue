14391f6bc242c1d14f60a1d9b73f7462fc55fe43
WilGero <wil.gero.2020@gmail.com>
2023-04-27 20:43:09 -0400

    initial

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Holaaa desde el componente Counter</h2>
+</template>
+
+<script>
+export default {
+    name:Counter,
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
